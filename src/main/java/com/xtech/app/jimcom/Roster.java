139b20e0fea31a1def03964de9409ea258f6ab60
diggit <patrikbachan@gmail.com>
2014-05-01 12:25:46 +0200

    added test code, IP conversion, improved incomming connection handling

--- src/main/java/com/xtech/app/jimcom/Roster.java
+++ src/main/java/com/xtech/app/jimcom/Roster.java
@@ -33,28 +33,62 @@ public class Roster extends Thread
 
 	private Identity local;
 	private volatile List<Contact> contactList;
+	private final List<String> args;
 
 	GuiFXController controller;
-	Roster(GuiFXController controller)
+	Roster(GuiFXController controller,List<String> args)
 	{
 		if(controller==null)
 			throw new NullPointerException("refference to controller was null!");
 		this.controller=controller;
 		contactList=new ArrayList();
-		local=new SimpleIdentity("xorly","somehash");
+		if(args.contains("-Atest"))
+		{
+			local=new SimpleIdentity("Atest","somecoolhash");
+		}
+		else if (args.contains("-Btest"))
+		{
+			local=new SimpleIdentity("Btest","somehothash");
+		}
+		else
+			local=new SimpleIdentity("xorly","somehash");
+		this.args=args;
+
+		shout("args...");
+		for (String arg : args) {
+			System.out.println(arg);
+		}
+	}
+
+	public List<String> getArgs()
+	{
+		return args;
 	}
 
 
 	public void run()
 	{
+		shout("started!");
 		loadStoredContacts();
 
 		shout("testing...");
 
+		Listener listener;
+		if(args.contains("-Atest"))
+		{
+			shout("setting up as node A");
+			listener=new Listener(this,5564,convertAddress(192, 168, 1, 128));
 
+
+		}
+		else if (args.contains("-Btest"))
+		{
+			shout("setting up as node B");
+			listener=new Listener(this,5564,convertAddress(192, 168, 1, 139));
+		}
         
-
-        Listener listener=new Listener(this,5564);
+		else
+        	listener=new Listener(this,5564);
         listener.start();//start listener to receive incomming connections
 
         serve();
@@ -64,7 +98,8 @@ public class Roster extends Thread
 	{
 		while(true)
 		{
-			shout("updatting contact list...");
+			shout("updating contact list...");
+			//TODO: (40) do something useful during refresh
 			for (Contact c : contactList)
 			{
 				shout("contact: "+c.getNickname());
@@ -83,21 +118,54 @@ public class Roster extends Thread
 	}
 
 	
-
+	public static InetAddress convertAddress(int a,int b,int c,int d)
+	{
+		if(a<0||a>255||b<0||b>255||c<0||c>255||d<0||d>255)
+		{
+			shout("IP number out of range 0~255 !");
+			return null;
+		}
+		else
+		{
+			if(a>127)
+				a-=256;
+			if(b>127)
+				b-=256;
+			if(c>127)
+				c-=256;
+			if(d>127)
+				d-=256;
+			//omfg why this shit has bytes from -128 to 127 ?!
+			try
+			{
+				return InetAddress.getByAddress(new byte[]{(byte)a,(byte)b,(byte)c,(byte)d});
+			}
+			catch(UnknownHostException ex)
+			{
+				shout("UnknownHostException");
+				return null;
+			}
+		}
+	}
 	public void loadStoredContacts()
 	{
-		
-		try
+		Contact c3;
+		if(args.contains("-Atest"))
 		{
-			// Contact c2=new Contact("FLOLED","none",InetAddress.getByAddress(new byte[]{127, 0, 0, 1}),5564,local);
-			// addContact(c2);
-
-			Contact c3=new Contact("xorly","somehash",InetAddress.getByAddress(new byte[]{127, 0, 0, 1}),5564,local);
-			addContact(c3);
-
+			c3=new Contact("Btest","somehothash",convertAddress(192, 168, 1, 139),5564,local);
 		}
-		catch(UnknownHostException ex)
-			{shout("wrong ip format!");}
+		else if (args.contains("-Btest"))
+		{
+			c3=new Contact("Atest","somecoolhash",convertAddress(192, 168, 1, 128),5564,local);
+		}
+
+		// c2=new Contact("FLOLED","none",convertAddress(127, 0, 0, 1}),5564,local);
+		// addContact(c2);
+		else
+			c3=new Contact("xorly","somehash",convertAddress(127, 0, 0, 1),5564,local);
+		addContact(c3);
+
+
 		shout("all contacts loaded!");
         
 	}
@@ -186,26 +254,43 @@ public class Roster extends Thread
 					if(in.readLine().trim().equals(Protocol.TRANSMISSION_TAIL))//correctly closed tags
 					{
 						shout("dataframe properly closed, YEAH!");
-						int index=contactList.indexOf(incommingIdentity);
-						if(index>=0)//if identity is already in list
+						if (local.equals(incommingIdentity))
 						{
-							shout("contact "+incommingIdentity+" exists in database, binding socket...");
-							//TODO: (10) send OK response with ID
-							out.println(Protocol.authResponseAccept(local));
-							//contactList.get(index).bindSocket(incommingConnection);	
+							shout("this is WEIRD, LOOPBACK?");
+							out.println(Protocol.authResponseReject());
+							return false;
 						}
 						else
 						{
-							shout("contact "+incommingIdentity+" not yet known");
-							//TODO: (20) do some decisions, accept or not? user interraction needed - GUI
-							this.addContact(new Contact(incommingIdentity,incommingConnection,local));
+							int index=contactList.indexOf(incommingIdentity);
+							if(index>=0)//if identity is already in list
+							{
+								shout("contact "+incommingIdentity+" exists in database...");
+								out.println(Protocol.authResponseAccept(local));
+								if(contactList.get(index).isOnline())
+									shout("contact already online!");
+								else
+								{
+									shout("binding socket and bringing online!");
+									//shout("WARNING: binding disabled!");
+									contactList.get(index).bindSocket(incommingConnection);	
+								}
+
+								
+							}
+							else
+							{
+								shout("contact "+incommingIdentity+" not yet known");
+								//TODO: (20) do some decisions, accept or not? user interraction needed - GUI
+								this.addContact(new Contact(incommingIdentity,incommingConnection,local));
+							}
+							updateAvailability();
+							return true;
 						}
-						updateAvailability();
-						return true;
+						
 					}
 				}
 
-					
 			}
 			
 			shout("wrong protocol format!");
@@ -216,12 +301,8 @@ public class Roster extends Thread
 		}
 		
 		return false;
-
-		
-
-		
 	}
-	private void shout(String text)
+	private static void shout(String text)
 	{
 		//opt TODO: (90) use logger
 		System.out.println("ROSTER: "+text);
